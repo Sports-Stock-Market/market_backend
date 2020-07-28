db1d23951873d2435e1c9806f64def9b60f658f3
Dant86 <vedantdpathak@iCloud.com>
2020-07-28 19:32:40 +0100

    postgres

--- fanbasemarket/__init__.py
+++ fanbasemarket/__init__.py
@@ -21,52 +21,10 @@ import time
 import csv
 import math
 
-INJURIES = {'2019-11-01':[['Paul George', 'Clippers', 'Month']],
-    '2019-10-25': [['Deandre Ayton', 'Suns', 'Month']],
-    '2019-11-02':[['Stephen Curry', 'Warriors', 'Season']], 
-    '2019-12-18':[['Karl-Anthony Towns', 'Timberwolves', 'Month']], 
-    '2020-02-12':[['Karl-Anthony Towns', 'Timberwolves', 'Year']], 
-    '2020-02-05':[['Tyler Herro', 'Heat', 'Month']], 
-    '2019-11-16':[['Kyrie Irving', 'Nets', 'Month']],
-    '2019-12-20':[['Norman Powell', 'Raptors', 'Month']], 
-    '2019-10-23':[['Marvin Bagley III', 'Kings', 'Month']],
-    '2020-01-22':[['Marvin Bagley III', 'Kings', 'Season']],
-    '2020-01-24':[['Lauri Markkanen', 'Bulls', 'Month']], 
-    '2020-01-31':[['Clint Capela', 'Hawks', 'Season'], ['Marc Gasol', 'Raptors', 'Season']], 
-    '2019-11-15':[['Jonathan Isaac', 'Magic', 'Month']], 
-    '2020-01-01':[['Jonathan Isaac', 'Magic', 'Season']], 
-    '2020-03-05':[['Bradley Beal', 'Wizards', 'Season'], ['DeAndre Jordan', 'Nets', 'Season']],
-    '2019-12-08':[['Rodney Hood', 'Trail Blazers', 'Season']],
-    '2020-03-06':[['Pascal Siakam', 'Raptors', 'Season']],
-    '2019-11-16':[['Nikola Jokic', 'Nuggets', 'Season']]
-}
-
-STARTING_ELOS = {
-    '76ers': 1525, 'Bucks': 1575, 'Bulls': 1200, 'Cavaliers': 1000, 
-    'Celtics': 1455, 'Clippers': 1600, 'Grizzlies': 1100, 'Hawks': 1200, 
-    'Heat': 1325, 'Hornets': 1050, 'Jazz': 1475, 'Kings': 1250, 
-    'Knicks': 1200, 'Lakers': 1580, 'Magic': 1200, 'Mavericks': 1300, 
-    'Nets': 1350, 'Nuggets': 1450, 'Pacers': 1300, 'Pelicans': 1250,
-    'Pistons': 1100, 'Raptors': 1345, 'Rockets': 1525, 'Spurs': 1300, 'Suns' : 1250,
-    'Thunder': 1250, 'Timberwolves': 1200, 'Trail Blazers': 1415, 'Warriors': 1400,
-    'Wizards': 1200
-}
-
-def get_starting_elo(tname):
-    for k in STARTING_ELOS.keys():
-        if k in tname:
-            return STARTING_ELOS[k]
-
-load_dotenv()
-db_usr = getenv('DB_USR')
-db_pass = getenv('DB_PASS')
-db_host = getenv('DB_HOST')
-db_name = getenv('DB_NAME')
-
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_usr}:{db_pass}@{db_host}/{db_name}?ssl=false'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['JWT_TOKEN_LOCATION'] = ['cookies', 'headers']
 app.config['JWT_SECRET_KEY'] = getenv('API_SECRET')
